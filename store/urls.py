0956e2175e8ae3e8d0f039273604773710618898
AnnualBore8 <ars_e-maxx@mail.ru>
2023-07-01 01:01:31 +0300

    everything work full end of practice 2

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import ProductSingleView, CartView, ShopView
+from .views import ProductView, CartView, ShopView
 
 app_name = 'store'
 
 urlpatterns = [
     path('', ShopView.as_view(), name='shop'),
     path('cart/', CartView.as_view(), name='cart'),
-    path('product/<int:id>/', ProductSingleView.as_view(), name='product'),
+    path('product/<int:id>/', ProductView.as_view(), name='product'),
 
 ]
--- store/views.py
+++ store/views.py
@@ -38,100 +38,20 @@ class ShopView(View):
         return render(request, 'store/shop.html', {"data": products})
 
 
-class ProductSingleView(View):
+class ProductView(View):
     def get(self, request, id):
-        data = {1: {'name': 'Bell Pepper',
-                    'description': 'Bell Pepper',
-                    'price': 120.00,
-                    'rating': 5.0,
-                    'url': 'store/images/product-1.jpg'},
-                2: {'name': 'Strawberry',
-                    'description': 'Strawberry',
-                    'price': 120.00,
-                    'rating': 5.0,
-                    'url': 'store/images/product-2.jpg'},
-                3: {'name': 'Green Beans',
-                    'description': 'Green Beans',
-                    'price': 120.00,
-                    'rating': 5.0,
-                    'url': 'store/images/product-3.jpg'},
-                4: {'name': 'Purple Cabbage',
-                    'description': 'Purple Cabbage',
-                    'price': 120.00,
-                    'rating': 5.0,
-                    'url': 'store/images/product-4.jpg'},
-                5: {'name': 'Tomatoes',
-                    'description': 'Tomatoes',
-                    'price': 120.00,
-                    'rating': 5.0,
-                    'url': 'store/images/product-5.jpg'},
-                6: {'name': 'Brocolli',
-                    'description': 'Brocolli',
-                    'price': 120.00,
-                    'rating': 5.0,
-                    'url': 'store/images/product-6.jpg'},
-                7: {'name': 'Carrots',
-                    'description': 'Carrots',
-                    'price': 120.00,
-                    'rating': 5.0,
-                    'url': 'store/images/product-7.jpg'},
-                8: {'name': 'Fruit Juice',
-                    'description': 'Fruit Juice',
-                    'price': 120.00,
-                    'rating': 5.0,
-                    'url': 'store/images/product-8.jpg'},
-                9: {'name': 'Onion',
-                    'description': 'Onion',
-                    'price': 120.00,
-                    'rating': 5.0,
-                    'url': 'store/images/product-9.jpg'},
-                10: {'name': 'Apple',
-                     'description': 'Apple',
-                     'price': 120.00,
-                     'rating': 5.0,
-                     'url': 'store/images/product-10.jpg'},
-                11: {'name': 'Garlic',
-                     'description': 'Garlic',
-                     'price': 120.00,
-
-                     'rating': 5.0,
-                     'url': 'store/images/product-11.jpg'},
-                12: {'name': 'Chilli',
-                     'description': 'Chilli',
-                     'price': 120.00,
-                     'rating': 5.0,
-                     'url': 'store/images/product-12.jpg'}
-                }
-        return render(request, 'store/product-single.html', context=data[id])
+        data = Product.objects.get(id=id)
+        return render(request, 'store/product-single.html', context={'name': data.name,
+                                                                     'description': data.description,
+                                                                     'price': data.price,
+                                                                     'rating': 5.0,
+                                                                     'url': data.image.url})
 
 
 class CartView(View):
     def get(self, request):
         return render(request, 'store/cart.html')
 
-
-# class ShopView(View):
-#     def get(self, request):
-#         discount_value = Case(When(discount__value__gte=0,
-#                                    discount__date__begin__lte=timezone.now(),
-#                                    discount__date_end__gte=timezone.now(),
-#                                    then=F('discount__value')),
-#                               default=0,
-#                               output_field=DecimalField(max_digits=10, decimal_places=2)
-#                               )
-#
-#         price_with_discount = ExpressionWrapper(
-#             F('price') * (100.0 - F('discount_value')) / 100.0,
-#             output_field=DecimalField(max_digits=10, decimal_places=2)
-#         )
-#
-#         products = Product.objects.annotate(
-#             discount_value=discount_value,
-#             price_before=F('price'),
-#             price_after=price_with_discount
-#         ).values('id', 'name', 'image', 'price_before', 'price_after', 'discount_value')
-#
-#         return render(request, 'store/shop.html', {'data': products})
 
 #  Старая версия реализации добавления продуктов через словарь в файле Питона.
 #  Мы ее делали, чтобы просто посмотреть, что так можно, на практике так почти не делают, потому что это не удобно.
@@ -204,3 +124,70 @@ class CartView(View):
 #             ]
 #         }
 #         return render(request, 'store/shop.html', context)
+
+
+# class ProductView(View):
+#     def get(self, request, id):
+#         data = {1: {'name': 'Bell Pepper',
+#                     'description': 'Bell Pepper',
+#                     'price': 120.00,
+#                     'rating': 5.0,
+#                     'url': 'store/images/product-1.jpg'},
+#                 2: {'name': 'Strawberry',
+#                     'description': 'Strawberry',
+#                     'price': 120.00,
+#                     'rating': 5.0,
+#                     'url': 'store/images/product-2.jpg'},
+#                 3: {'name': 'Green Beans',
+#                     'description': 'Green Beans',
+#                     'price': 120.00,
+#                     'rating': 5.0,
+#                     'url': 'store/images/product-3.jpg'},
+#                 4: {'name': 'Purple Cabbage',
+#                     'description': 'Purple Cabbage',
+#                     'price': 120.00,
+#                     'rating': 5.0,
+#                     'url': 'store/images/product-4.jpg'},
+#                 5: {'name': 'Tomatoes',
+#                     'description': 'Tomatoes',
+#                     'price': 120.00,
+#                     'rating': 5.0,
+#                     'url': 'store/images/product-5.jpg'},
+#                 6: {'name': 'Brocolli',
+#                     'description': 'Brocolli',
+#                     'price': 120.00,
+#                     'rating': 5.0,
+#                     'url': 'store/images/product-6.jpg'},
+#                 7: {'name': 'Carrots',
+#                     'description': 'Carrots',
+#                     'price': 120.00,
+#                     'rating': 5.0,
+#                     'url': 'store/images/product-7.jpg'},
+#                 8: {'name': 'Fruit Juice',
+#                     'description': 'Fruit Juice',
+#                     'price': 120.00,
+#                     'rating': 5.0,
+#                     'url': 'store/images/product-8.jpg'},
+#                 9: {'name': 'Onion',
+#                     'description': 'Onion',
+#                     'price': 120.00,
+#                     'rating': 5.0,
+#                     'url': 'store/images/product-9.jpg'},
+#                 10: {'name': 'Apple',
+#                      'description': 'Apple',
+#                      'price': 120.00,
+#                      'rating': 5.0,
+#                      'url': 'store/images/product-10.jpg'},
+#                 11: {'name': 'Garlic',
+#                      'description': 'Garlic',
+#                      'price': 120.00,
+#
+#                      'rating': 5.0,
+#                      'url': 'store/images/product-11.jpg'},
+#                 12: {'name': 'Chilli',
+#                      'description': 'Chilli',
+#                      'price': 120.00,
+#                      'rating': 5.0,
+#                      'url': 'store/images/product-12.jpg'}
+#                 }
+#         return render(request, 'store/product-single.html', context=data[id])
